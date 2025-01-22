134a1b7cd103601f6546956fe58479ad3115375d
mwantech <mwangijoshua283@gmail.com>
2025-01-22 11:41:51 +0300

    redesigning app css

--- frontend/src/auth/Login.jsx
+++ frontend/src/auth/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import "./Login.css";
+
 
 const Login = ({ onLoginSuccess }) => {
   const [email, setEmail] = useState("");
--- frontend/src/components/Chatinterface.jsx
+++ frontend/src/components/Chatinterface.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import "./ChatInterface.css";
+
 
 const ChatInterface = () => {
   const [messages, setMessages] = useState([]);
