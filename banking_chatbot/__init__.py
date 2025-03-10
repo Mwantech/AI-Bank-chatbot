bc4b49ab78b1903eac8b7c985a9bc115355ff1e3
mwantech <mwangijoshua283@gmail.com>
2025-03-10 15:33:24 +0300

    removing unnecessary files

--- banking_chatbot/__init__.py
+++
@@ -1,5 +0,0 @@
-# bank_chatbot/__init__.py
-from services.Banking_chatbot import BankingChatbot
-from services.Nlp_service import BankingNLPService
-
-__all__ = ['BankingChatbot', 'BankingNLPService']
